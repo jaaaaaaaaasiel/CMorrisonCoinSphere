0c40f55f493f2d7f56f3e04a299a10d419f722ba
Jaasiel Morrison <jaaaaaaaaasiel@gmail.com>
2025-09-11 19:52:15 -0600

    Implementación y Prueba de Colores

--- app/src/main/java/com/example/coinspheremorrison/MainActivity.kt
+++ app/src/main/java/com/example/coinspheremorrison/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coinspheremorrison.ui.theme.Background
 import com.example.coinspheremorrison.ui.theme.CoinSphereMorrisonTheme
+import com.example.coinspheremorrison.ui.theme.Surface
+import com.example.coinspheremorrison.ui.theme.TextDim
+import com.example.coinspheremorrison.ui.theme.TextMain
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoinSphereMorrisonTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting(innerPadding)
                 }
             }
         }
@@ -31,17 +37,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(innerpadding: PaddingValues) {
+    Column (modifier = Modifier 
+        .background(Background)
+        .fillMaxSize()
+    ) {
+        Text(
+            text = "Prueba Colores",
+            color = TextMain,
+            modifier  = Modifier.weight(2f)
+        )
+
+        Row (modifier = Modifier
+            .background(Surface)
+            .fillMaxWidth()
+            .weight(1f)
+        ) {
+            Text("Surface", color = TextDim)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CoinSphereMorrisonTheme {
-        Greeting("Android")
+        Greeting(PaddingValues(1.dp))
     }
 }
